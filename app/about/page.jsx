d541855ee40dbf467496c7f309a811d33dc5faf2
Suleman Babar <sultansuleman892@gmail.com>
2025-11-25 15:23:18 +0500

    add padding in about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -8,7 +8,7 @@ const About = () => {
           About Me
         </h1>
         <div className="  mt-3 bg-white rounded-xl shadow hover:shadow-xl transition in-dark:bg-gray-300 ">
-          <p className="pl-6 pt-3 pb-2 text-gray-700 leading-relaxed ">
+          <p className="pl-6 pt-10 pb-10 pr-10 text-gray-700 leading-relaxed ">
          I am Salman Babar, a beginner Frontend Developer with 3 months of experience. I build clean and responsive websites using HTML, CSS, JavaScript, React.js, Next.js, and Tailwind CSS. I enjoy learning new technologies and improving my skills by making real projects.
           </p>
         </div>
